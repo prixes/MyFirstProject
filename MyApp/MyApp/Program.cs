7d80efa49583f45a6d2daa41ab7ff32d90854bac
David Parushev <david.parushev@blankfactor.com>
2024-11-14 19:24:39 +0200

    define Numbers and String

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,4 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyApp.Types;
+
+
+
+var numbers = new Numbers();
+
+
+Console.WriteLine(numbers.firstNumber);
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 //Console.WriteLine("Hello, World!");
