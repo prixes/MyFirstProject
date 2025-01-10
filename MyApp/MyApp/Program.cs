e2d3d5e5f30d83172971a8eb6904ac5463d40d33
David Parushev <david.parushev@blankfactor.com>
2025-01-10 10:46:26 +0200

    lecture 14 materials

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,16 +1,81 @@
-﻿using MyApp.Types;
+﻿
+
+using MyApp.Types;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text.Json.Nodes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Program
+{
+    public static void Main()
+    {
+        //First
+        //string jsonSimple = "{ 'name': 'John', 'age': 30 }";
+        //var person = JsonConvert.DeserializeObject<Person>(jsonSimple);
+
+        //Console.WriteLine(person.Name); // Output: John
+
+        //Second
+        //string json = @"
+        //{
+        //    'employees': [
+        //        { 'name': 'Alice', 'age': 25 },
+        //        { 'name': 'Bob', 'age': 30 }
+        //    ]
+        //}";
+        ////string json = @"[1,2,3]"; // --- TEST for JObject
+
+        //JObject jsonObject = JObject.Parse(json);
+        ////string name = (string)jsonObject.SelectToken("$.employees[1].name");
+
+        //var older = jsonObject.SelectToken("$.employees[?(@.age > 27)]");
+
+        //Console.WriteLine($"{older["name"]} {older["age"]}"); // Output: Bob
+        ////Console.WriteLine(name); // Output: Bob
+
+
+        var jsonData = @"
+        {
+            'data': {
+                        'items': [
+                            { 'id': 1, 'value': 'A' },
+                            { 'id': 2, 'value': 'B' }
+                        ]
+            }
+        }
+        }";
+
+        JObject parsedData = JObject.Parse(jsonData);
+        var requireData = (string)parsedData.SelectToken("$.data.items[1].value");
+        Console.WriteLine(requireData);
+        
+
+        //string jsonBody = "{ 'name': 'John', 'skills': ['C#', 'JavaScript'] }";
+        //JToken token = JToken.Parse(jsonBody);
 
 
 
-var numbers = new Numbers();
+        //Console.WriteLine(token["name"]); // Output: John
+
+        ////Third
+        //string jsonUser = @"
+        //{
+        //    'user_id': 1,
+        //    'full_name': 'John Doe',
+        //    'email_address': 'john.doe@example.com'
+        //}";
+
+        //User user = JsonConvert.DeserializeObject<User>(jsonUser);
+
+        //Console.WriteLine($"Id: {user.Id}, Name: {user.Name}, Email: {user.Email}");
 
 
-Console.WriteLine(numbers.firstNumber);
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+        //var older = jsonObject.SelectToken("$.employees[?(@.age > 27)]");
+        //Console.WriteLine($"{older["name"]} {older["age"]}"); // Output: Bob
+    }
 
-//Console.WriteLine("Hello, World!");
-//Console.WriteLine("Hello, World!");
-//Console.WriteLine("Hello, World!");
+}
---
+++ MyApp/MyApp/Types/User.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+public class User
+{
+    [JsonProperty("user_id")]
+    public int Id { get; set; }
+
+    [JsonProperty("full_name")]
+    public string Name { get; set; }
+
+    [JsonProperty("email_address")]
+    public string Email { get; set; }
+}
+
+//public class User {
+//    public string Name { get; set; }
+
+//    [JsonExtensionData]
+//    public IDictionary<string, JToken> ExtraData { get; set; }
+//}
